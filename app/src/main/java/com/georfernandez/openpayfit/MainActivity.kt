a08ace4597bf5f97a7c3facf1cd316fffd8dd2a8
Georgina Fernandez <maria.fernandez@smiledirectclub.com>
2023-10-27 17:02:50 -0300

    Bottom Navigation added
    Bottom Navigation added

--- app/src/main/java/com/georfernandez/openpayfit/MainActivity.kt
+++ app/src/main/java/com/georfernandez/openpayfit/MainActivity.kt
@@ -2,10 +2,31 @@ package com.georfernandez.openpayfit
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.georfernandez.openpayfit.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navView: BottomNavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_profile,
+                R.id.navigation_movies,
+                R.id.navigation_location,
+                R.id.navigation_picture,
+            ),
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
