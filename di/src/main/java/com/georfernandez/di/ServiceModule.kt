da78e4d2a2cbdd540de25d4020c2e55a33fb105b
Georgina Fernandez <maria.fernandez@smiledirectclub.com>
2023-10-31 15:34:26 -0300

    Picture tab added

--- di/src/main/java/com/georfernandez/di/ServiceModule.kt
+++ di/src/main/java/com/georfernandez/di/ServiceModule.kt
@@ -1,7 +1,9 @@
 package com.georfernandez.di
 
+import com.georfernandez.data.service.FirebaseServiceImpl
 import com.georfernandez.data.service.TMDBServiceImpl
 import com.georfernandez.data.service.api.TMDBApi
+import com.georfernandez.domain.service.FirebaseService
 import com.georfernandez.domain.service.TMDBService
 import dagger.Module
 import dagger.Provides
@@ -13,4 +15,7 @@ import dagger.hilt.components.SingletonComponent
 object ServiceModule {
     @Provides
     fun provideTMDBService(tMDBApi: TMDBApi): TMDBService = TMDBServiceImpl(tMDBApi)
+
+    @Provides
+    fun provideFirebaseService(): FirebaseService = FirebaseServiceImpl()
 }
